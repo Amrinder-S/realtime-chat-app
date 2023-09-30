97acc5d3f7794f933b984f58f3e6a415e8b0c44b
AMR <amrSAMP@proton.me>
2023-09-30 21:06:35 +0530

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO
+import requests
+url = 'http://192.168.1.25/samp'
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('update_message')
+def update_message(new_message):
+    global message
+    message = new_message
+    socketio.emit('message_updated', message)
+
+@app.route('/update_message', methods=['POST'])
+def update_message_post():
+    global message
+    new_message = request.form.get('new_message')
+    if new_message:
+        message = new_message
+        #response = requests.post(url, data=message, headers={'Priority': 'high'})
+        socketio.emit('message_updated', message)
+        return jsonify({"message": "Message updated successfully"})
+    else:
+        return jsonify({"error": "New message not provided"}), 400
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=False, host='0.0.0.0')
